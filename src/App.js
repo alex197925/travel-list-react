c74eeffd4743375e09db2b7ecf5aef7bd9b40d91
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-12-04 12:12:47 +0100

    Add style to packed item conditionally

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 /** @format */
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 1, packed: true },
+];
+
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
@@ -11,8 +20,46 @@ function App() {
 function Logo() {
   return <h1>🌴 Far Away 🧳</h1>;
 }
-function Form() {}
-function PackingList() {}
-function Stats() {}
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  // console.log(item);
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>
+        🧳 You have XXX items on your list, and you already packed XX (XX%)
+      </em>
+    </footer>
+  );
+}
 
 export default App;
